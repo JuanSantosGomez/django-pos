25731416e23f2b90a9edeb65c182d0023043db62
unknown <juansantosgomez@protonmail.com>
2021-11-17 03:29:54 +0800

    Merged duplicates when creating Cart Items
    Modified the post method of GetCartItems so that when duplicates are
    punched in the quantities are added instead of creating a new item.
    
    Post method now returns all the items in the cart

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from store.models import Cart, CartItem
 from store.serializers import CartItemSerializer, CartSerializer
-from rest_framework import status
+from rest_framework import serializers, status
 
 # Create your views here.
 
@@ -15,11 +15,26 @@ class GetCartItems(APIView):
         return Response(serializer.data)
 
     def post(self, request, format=None, **kwargs):
-        serializer = CartItemSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        carts = Cart.objects.get(pk=self.kwargs['cart'])
+        cartitems = CartItem.objects.filter(cart=carts)
+        duped = False
+        for item in cartitems:
+            if item.id == request.data['product']:
+                item.quantity += request.data['quantity']
+                item.save()
+                duped = True
+
+        serializero = CartItemSerializer(cartitems, many=True)
+
+        if not duped:
+            serializer = CartItemSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+
+                return Response(serializero.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializero.data, status=status.HTTP_206_PARTIAL_CONTENT)
 
     def patch(self, request, format=None, **kwargs):
         cart = Cart.objects.get(pk=self.kwargs['cart'])
@@ -36,6 +51,6 @@ class GetCartItems(APIView):
 class CartItemDetail(APIView):
 
     def delete(self, request, pk, format=None):
-        cartitem = self.get_object(pk)
+        cartitem = CartItem.objects.get(pk=pk)
         cartitem.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
