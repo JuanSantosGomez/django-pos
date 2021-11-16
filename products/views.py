09436a18898fd415c7e29ff36985b9156594b0e7
unknown <juansantosgomez@protonmail.com>
2021-11-17 01:55:58 +0800

    deleted dead code on header

--- products/views.py
+++ products/views.py
@@ -1,12 +1,8 @@
-from django.shortcuts import render
-from rest_framework import views
-from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from products.serializers import ProductSerializer
-from products.models import Product
-import os
+
 
 # Create your views here.
 
